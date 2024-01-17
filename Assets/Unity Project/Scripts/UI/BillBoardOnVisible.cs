41a5aa2f4ea6ed804ffcd8cb40237c331d0b7afc
NoahCedeno <noahcedeno1122@gmail.com>
2024-01-17 11:49:47 -0500

    DEV - Implemented Object UI Pins
    I made little billboards so that you can better see the SelectableObjects!
    :llama:

---
+++ Assets/Unity Project/Scripts/UI/BillBoardOnVisible.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Rotates the Object towards the Player while the sprite is visible by the cameras.
+/// </summary>
+public class BillBoardOnVisible : MonoBehaviour
+{
+    private bool m_IsVisible = false;
+
+    private SpriteRenderer m_Renderer;
+
+    private Camera m_MainCamera;
+
+    private void Awake()
+    {
+        m_Renderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        m_MainCamera = Camera.main;
+    }
+
+    private void OnBecameVisible()
+    {
+        m_IsVisible = true;
+    }
+
+    private void OnBecameInvisible()
+    {
+        m_IsVisible = false;
+    }
+
+    private void LateUpdate()
+    {
+        if (!m_IsVisible) return;
+
+        // If we're VISIBLE,
+        transform.LookAt(m_MainCamera.transform);
+        transform.Rotate(new Vector3(0f, 180f, 0f));
+    }
+}
